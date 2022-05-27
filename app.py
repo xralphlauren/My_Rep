d4423796a8e1686c67463611e1df6619bc117c43
xralphlauren <dsmyronchuk@gmail.com>
2022-05-27 21:23:12 +0300

    commit for branch

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ for i in range(15):
     print(i)
     
     
-[print(i) for i in 'prived']
+a = [i for i in 'prived']
+
+
+if len(a) > 5:
+    print(f'строка равна {len(a)} символам'
+    
+print('i say hello')
+
+print('respect')
